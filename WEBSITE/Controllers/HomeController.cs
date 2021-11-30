c677fe335ca11c8d66f09bccb39a4be79c1d5143
Tùng Trần Thanh <tungtranthanh258@gmail.com>
2021-11-30 12:07:56 +0700

    create new website

---
+++ WEBSITE/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using WEBSITE.Models;
+using DAL.Entities;
+
+namespace WEBSITE.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
