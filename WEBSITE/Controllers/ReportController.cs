c0c5ba6a1517992336713f7760420ddc8b2d2b69
Tùng Trần Thanh <tungtranthanh258@gmail.com>
2021-12-14 16:48:22 +0700

    Report for webform

---
+++ WEBSITE/Controllers/ReportController.cs
@@ -0,0 +1,37 @@
+﻿using BUS;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEBSITE.Models;
+
+namespace WEBSITE.Controllers
+{
+    public class ReportController : Controller
+    {
+        private BUS_TouristGroup bTouristGroup = new BUS_TouristGroup();
+        private BUS_Tour bTour = new BUS_Tour();
+        public IActionResult Index()
+        {
+            ReportViewModel reportViewModel = new ReportViewModel
+            {
+                TouristGroups = bTouristGroup.GetAll(),
+                Tours = bTour.GetAll()
+            };
+            return View(reportViewModel);
+        }
+        
+        [HttpGet]
+        public ActionResult DrawChart(int id)
+        {
+            //get list touristGroup has Tourid equals with id
+            var listTouristGroup = bTouristGroup.GetAll().Where(x => x.TourId == id).ToList();
+            if (listTouristGroup == null)
+            {
+                return NotFound();
+            }
+            return PartialView("_DrawChart", listTouristGroup);
+        }
+    }
+}
---
+++ WEBSITE/Models/ReportViewModel.cs
@@ -0,0 +1,14 @@
+﻿using DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WEBSITE.Models
+{
+    public class ReportViewModel
+    {
+        public List<Tour> Tours { get; set; }
+        public List<TouristGroup> TouristGroups { get; set; }
+    }
+}
